461fce7398770698802683464775559004442f37
Matthew Little <zone117x@gmail.com>
2023-05-24 08:43:41 -0600

    chore: place signing code in one spot

--- src/message/preamble.ts
+++ src/message/preamble.ts
@@ -2,8 +2,6 @@ import { ResizableByteStream } from '../resizable-byte-stream';
 import { Encodeable } from '../stacks-p2p-deser';
 import { MessageSignature } from './message-signature';
 import { BurnchainHeaderHash } from './burnchain-header-hash';
-import { createHash, randomBytes } from 'node:crypto';
-import * as secp256k1 from 'secp256k1';
 
 export class Preamble implements Encodeable {
   /**
@@ -115,37 +113,5 @@ export class Preamble implements Encodeable {
     target.writeUint32(this.additional_data);
     this.signature.encode(target);
     target.writeUint32(this.payload_len);
-  }
-
-  /**
-   * Based on https://github.com/stacks-network/stacks-blockchain/blob/master/src/net/codec.rs#L88
-   * And SIP-003: https://github.com/stacksgov/sips/blob/main/sips/sip-003/sip-003-peer-network.md#creating-a-control-plane-message
-   *
-   * All control-plane messages are signed with the node's session private key using ECDSA on the secp256k1 curve. To sign a StacksMessage, a peer uses the following algorithm:
-   * 1. Serialize the payload to a byte string.
-   * 2. Set the preamble.payload_len field to the length of the payload byte string
-   * 3. Set the preamble.seq field to be the number of messages sent to this peer so far.
-   * 4. Set the preamble.signature field to all 0's
-   * 5. Serialize the preamble to a byte string.
-   * 6. Calculate the SHA512/256 over the preamble and payload byte strings
-   * 7. Calculate the recoverable secp256k1 signature from the SHA256
-   */
-  sign(privKey: Buffer, envelopeStream: ResizableByteStream): void {
-    this.signature = MessageSignature.empty();
-
-    // Zero-out the old signature so we can calculate a new one.
-    const preambleStream = new ResizableByteStream();
-    this.encode(preambleStream);
-    const sha512_256 = createHash('sha512-256')
-      .update(preambleStream.asBuffer())
-      .update(envelopeStream.asBuffer())
-      .digest();
-
-    const signature = secp256k1.ecdsaSign(sha512_256, privKey);
-    const buf1 = Buffer.alloc(1);
-    buf1.writeUint8(signature.recid);
-    this.signature = new MessageSignature(
-      Buffer.concat([buf1, signature.signature]).toString('hex')
-    );
   }
 }
--- src/message/stacks-message-envelope.ts
+++ src/message/stacks-message-envelope.ts
@@ -1,7 +1,11 @@
+import { createHash } from 'node:crypto';
+import * as secp256k1 from 'secp256k1';
+
 import { Preamble } from './preamble';
 import { ResizableByteStream } from '../resizable-byte-stream';
 import { Encodeable, StacksMessageTypedContainer } from '../stacks-p2p-deser';
 import { RelayDataVec } from './relay-data';
+import { MessageSignature } from './message-signature';
 
 /**
  * This is called just "StacksMessage" in the SIP, using "envelope" for disambiguation.
@@ -39,7 +43,20 @@ export class StacksMessageEnvelope implements Encodeable {
     this.payload.encode(target);
   }
 
-  // Based on https://github.com/stacks-network/stacks-blockchain/blob/master/src/net/codec.rs#L1120
+  /**
+   * Based on https://github.com/stacks-network/stacks-blockchain/blob/master/src/net/codec.rs#L88
+   *      and https://github.com/stacks-network/stacks-blockchain/blob/master/src/net/codec.rs#L1120
+   * And SIP-003: https://github.com/stacksgov/sips/blob/main/sips/sip-003/sip-003-peer-network.md#creating-a-control-plane-message
+   *
+   * All control-plane messages are signed with the node's session private key using ECDSA on the secp256k1 curve. To sign a StacksMessage, a peer uses the following algorithm:
+   * 1. Serialize the payload to a byte string.
+   * 2. Set the preamble.payload_len field to the length of the payload byte string
+   * 3. Set the preamble.seq field to be the number of messages sent to this peer so far.
+   * 4. Set the preamble.signature field to all 0's
+   * 5. Serialize the preamble to a byte string.
+   * 6. Calculate the SHA512/256 over the preamble and payload byte strings
+   * 7. Calculate the recoverable secp256k1 signature from the SHA256
+   */
   sign(privKey: Buffer): void {
     if (this.relayers.length > 0) {
       throw new Error('Can not sign a relayed message');
@@ -50,6 +67,21 @@ export class StacksMessageEnvelope implements Encodeable {
     this.payload.encode(contentStream);
     this.preamble.payload_len = contentStream.position;
 
-    this.preamble.sign(privKey, contentStream);
+    // Zero-out the old signature so we can calculate a new one.
+    this.preamble.signature = MessageSignature.empty();
+
+    const preambleStream = new ResizableByteStream();
+    this.preamble.encode(preambleStream);
+    const sha512_256 = createHash('sha512-256')
+      .update(preambleStream.asBuffer())
+      .update(contentStream.asBuffer())
+      .digest();
+
+    const signature = secp256k1.ecdsaSign(sha512_256, privKey);
+    const buf1 = Buffer.alloc(1);
+    buf1.writeUint8(signature.recid);
+    this.preamble.signature = new MessageSignature(
+      Buffer.concat([buf1, signature.signature]).toString('hex')
+    );
   }
 }
